256a6dbc9c550539e145ed11c4e44cae01d29743
Shixiong Qi <qishixiong19931009@gmail.com>
2020-03-26 22:16:46 -0700

    update

--- tracepkt.c
+++ tracepkt.c
@@ -281,7 +281,7 @@ int kretprobe__ipt_do_table(struct pt_regs *ctx)
 {
     return __ipt_do_table_out(ctx);
 }
-
+/*
 int kprobe__ip6t_do_table(struct pt_regs *ctx, struct sk_buff *skb, const struct nf_hook_state *state, struct xt_table *table)
 {
     return __ipt_do_table_in(ctx, skb, state, table);
@@ -291,3 +291,4 @@ int kretprobe__ip6t_do_table(struct pt_regs *ctx)
 {
     return __ipt_do_table_out(ctx);
 }
+*/
